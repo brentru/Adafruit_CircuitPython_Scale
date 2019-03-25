7f380ef9bbcf0e7b2438c03ceb4e8e75dfb677fb
brentru <robots199@me.com>
2019-03-25 12:28:17 -0400

    split apart error checking

--- adafruit_scale.py
+++ adafruit_scale.py
@@ -45,7 +45,7 @@ from pulseio import PulseIn
 
 OUNCES = const(0x0B)   # data in weight is in ounces
 GRAMS = const(0x02)    # data in weight is in grams
-PULSEWIDTH = 72.5
+PULSE_WIDTH = 72.5
 
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_scale.git"
@@ -63,7 +63,7 @@ class Scale:
         try:
             self.check_scale()
         except RuntimeError as e:
-            raise RuntimeError("Failed to inititalize the scale - is the scale on?")
+            raise RuntimeError("Failed to inititalize the scale, is the scale on?")
         # units we're measuring
         self.units = None
         # is the measurement stable?
@@ -78,7 +78,6 @@ class Scale:
         self.dymo.pause()
         self.dymo.clear()
         self.dymo.resume()
-
         while len(self.dymo) < 35:
             if (time.monotonic() - timestamp) > self.timeout:
                 raise RuntimeError("Timed out waiting for data")
@@ -94,25 +93,26 @@ class Scale:
         bit_idx = 0       # we will count a bit at a time
         bit_val = False   # first pulses will be LOW
         for i in range(len(self.dymo)):
-            if self.dymo[i] == 65535:     # This is the pulse between transmits
+            if self.dymo[i] == 65535: # check for the pulse between transmits
                 break
-            num_bits = int(self.dymo[i] / PULSEWIDTH + 0.5)  # ~14KHz == ~7.5us per clock
+            num_bits = int(self.dymo[i] / PULSE_WIDTH + 0.5)  # ~14KHz == ~7.5us per clock
             for bit in range(num_bits):
                 bits[bit_idx] = bit_val
                 bit_idx += 1
                 if bit_idx == 96:      # we have read all the data we wanted
                     break
             bit_val = not bit_val
-        data_bytes = [0] * 12
+        data_bytes = [0] * 12 # alllocate data array
         for byte_n in range(12):
-            thebyte = 0
+            the_byte = 0
             for bit_n in range(8):
-                thebyte <<= 1
-                thebyte |= bits[byte_n*8 + bit_n]
-            data_bytes[byte_n] = thebyte
+                the_byte <<= 1
+                the_byte |= bits[byte_n*8 + bit_n]
+            data_bytes[byte_n] = the_byte
         # do some very basic data checking
-        if data_bytes[0] != 3 or data_bytes[1] != 3 or data_bytes[7] != 4 \
-        or data_bytes[8] != 0x1C or data_bytes[9] != 0 or data_bytes[10] \
+        if data_bytes[0] != 3 or data_bytes[1] != 3 or data_bytes[7] != 4:
+            raise RuntimeError("Bad data capture")
+        if data_bytes[8] != 0x1C or data_bytes[9] != 0 or data_bytes[10] \
         or data_bytes[11] != 0:
             raise RuntimeError("Bad data capture")
 
@@ -126,8 +126,6 @@ class Scale:
             if data_bytes[4] & 0x80:
                 data_bytes[4] -= 0x100
             self.weight *= 10 ** data_bytes[4]
-        # convert units to readable string
-        if self.units == OUNCES:
             self.units = "oz"
-        elif self.units == GRAMS:
+        if self.units == GRAMS:
             self.units = "g"
